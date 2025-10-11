0bdc7a04f0afc831fb30d1b5f9406567ee912704
Stas Ratsa <stasratsa@gmail.com>
2025-10-11 13:39:22 +0300

    Final result

--- js/main.js
+++ js/main.js
@@ -1,12 +1,20 @@
 const themeBtn = document.querySelector('.header-theme');
 
-function changeAttribure (name, value) {
-    document.documentElement.setAttribute('name', 'value');
+function changeAttribute(name,value){
+    document.documentElement.setAttribute(name, value);
 }
 
+const savedTheme = localStorage.getItem('data-theme');
+if(savedTheme) {
+    changeAttribute('data-theme', savedTheme)
+}
+
+
 themeBtn.addEventListener('click', () => {
-    if(document.documentElement.getAttribute('data-theme') === 'dark') {
-        return changeAttribure('data-theme', 'light')
-    }
-    return changeAttribure('data-theme', 'dark')
+    const currentTheme = document.documentElement.getAttribute('data-theme');
+
+    const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
+    changeAttribute('data-theme', newTheme);
+    
+    localStorage.setItem('data-theme', newTheme);
 });
